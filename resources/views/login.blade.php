36767f39ed05bb35ebc241e7d900572246646601
Nelly-Komguep <komguepnelly55@gmail.com>
2025-09-11 17:14:56 +0100

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -72,7 +72,7 @@
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="{{ asset('js/login.js') }}"></script>
+<script src="{{ secure_asset('js/login.js') }}"></script>
 
   @if(session('success'))
     <script>
