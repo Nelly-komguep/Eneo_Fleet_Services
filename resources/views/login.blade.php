876a80279166e34de4369d47f7799582d0414bb5
Nelly-Komguep <komguepnelly55@gmail.com>
2025-09-11 17:06:08 +0100

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css"/>
-<link rel="stylesheet" href="{{ asset('css/styles.css')}}">
+<link rel="stylesheet" href="{{ secure_asset('css/styles.css')}}">
 <title>Login Eneo Fleet Service</title>
 </head>
 <body>
