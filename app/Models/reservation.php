abea0b2188a7fabfe13f4be8110f1fb359f683e8
Nelly-Komguep <komguepnelly55@gmail.com>
2025-09-15 14:34:57 +0100

    test

--- app/Models/reservation.php
+++ app/Models/reservation.php
@@ -3,16 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 
-class reservation extends Model
+class Reservation extends Model
 {
-    protected $fillable = ['type_reservation', 'date_depart', 'date_arrivee', 'lieu_depart', 'lieu_arrive', 'nombre_places', 'liste_passagers', 'motif', 'ordre_mission', 'statut', 'user_id'];
+    use HasFactory;
+
+    protected $fillable = [
+        'type_reservation',
+        'date_depart',
+        'date_arrivee',
+        'lieu_depart',
+        'lieu_arrive',
+        'nombre_places',
+        'liste_passagers',
+        'motif',
+        'ordre_mission',
+        'statut',
+        'user_id'
+    ];
 
     public function user()
-{
-    return $this->belongsTo(User::class);
+    {
+        return $this->belongsTo(User::class);
+    }
 }
-
-}
-
-
